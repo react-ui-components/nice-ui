f947b0f4ac76f51b31fea9bb620b61cd9eab8d13
liangxiaojie <liangxiaojie@genedock.com>
2017-07-01 11:18:29 +0800

    init example

---
+++ example/entry.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '../src/Button';
+
+main();
+
+function main() {
+  ReactDOM.render(<Button>test</Button>, document.getElementById('app'));
+};
