a2b2e10add9049d6d79c206859f80c3337b5735e
liangxiaojie <liangxiaojie@genedock.com>
2017-07-05 18:56:22 +0800

    clean

--- example/entry.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '../src/Button';
-
-main();
-
-function main() {
-  ReactDOM.render(<Button>test</Button>, document.getElementById('app'));
-};
