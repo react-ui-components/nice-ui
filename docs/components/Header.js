a6a9252132c53497c39d673d34e37d802fe53234
liangxiaojie <liangxiaojie@genedock.com>
2017-07-17 13:38:35 +0800

    Header

---
+++ docs/components/Header.js
@@ -0,0 +1,15 @@
+// @flow
+import Link from 'next/link'
+
+const Header = () => (
+  <div>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+    <Link href="/about">
+      <a>About</a>
+    </Link>
+  </div>
+)
+
+export default Header
