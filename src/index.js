fbd767628e3c12d294866ebe8e182fb1b7e16e5c
liangxiaojie <liangxiaojie@genedock.com>
2017-07-21 18:51:20 +0800

    export styled

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import * as colors from './colors'
 
 export { colors }
 
+export { default as styled } from './styled'
+
 export { default as Button } from './Button'
---
+++ src/styled.js
@@ -0,0 +1,2 @@
+// @flow
+export default from 'styled-components'
