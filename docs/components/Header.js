47f5e7b3c8ee6a493ea8f4a6ce631d09e0e305c2
liangxiaojie <liangxiaojie@genedock.com>
2017-07-21 18:19:34 +0800

    Header styled

--- docs/components/Header.js
+++ docs/components/Header.js
@@ -1,18 +1,44 @@
 // @flow
 import Link from 'next/link'
+import styled from 'nice-ui/styled'
+
+const Root = styled.div`
+  position: fixed;
+  top: 0;
+  left: auto;
+  right: 0;
+  width: 100%;
+  display: flex;
+  z-index: 1100;
+  flex-direction: column;
+`
+
+const ToolBar = styled.div`
+  height: 56px;
+  display: flex;
+  position: relative;
+  align-items: center;
+`
+
+const A = styled.a`
+  padding: 6px 12px;
+  margin-right: 10px;
+`
 
 const Header = () => (
-  <div>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <Link href="/components">
-      <a>Components</a>
-    </Link>
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-  </div>
+  <Root>
+    <ToolBar>
+      <Link href="/">
+        <A>Home</A>
+      </Link>
+      <Link href="/components">
+        <A>Components</A>
+      </Link>
+      <Link href="/about">
+        <A>About</A>
+      </Link>
+    </ToolBar>
+  </Root>
 )
 
 export default Header
