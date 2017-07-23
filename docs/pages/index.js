a8f2b6247c42541c5468f688a5dcf3ada04fe1cf
liangxiaojie <liangxiaojie@genedock.com>
2017-07-23 17:53:52 +0800

    index use nice-ui

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,26 +1,21 @@
 // @flow
 import styled from 'styled-components'
 import Markdown from 'react-markdown'
+import { ThemeProvider } from 'nice-ui/Theme'
+import { Button } from 'nice-ui'
 import Layout from '../components/Layout'
 import hello from '../mkdocs/hello.md'
 
-const Root = styled.div`
-  display: flex;
-  width: 100%;
-  min-height: 100vh;
-  align-items: stretch;
-`
-
 const Container = styled.div`
-  flex: '1 0 100%';
+  flex: 1 0 100%;
 `
 
 const Hero = styled.div`
-  min-height: '100vh';
-  flex: '0 0 auto';
-  display: 'flex';
-  justify-content: 'center';
-  align-items: 'center';
+  min-height: 100vh;
+  flex: 0 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Content = styled.div`
@@ -29,17 +24,18 @@ const Content = styled.div`
 `
 
 const Index = () => (
-  <Layout>
-    <Root>
+  <ThemeProvider>
+    <Layout>
       <Container>
         <Hero>
           <Content>
             <Markdown source={hello} />
+            <Button>GET STARTED</Button>
           </Content>
         </Hero>
       </Container>
-    </Root>
-  </Layout>
+    </Layout>
+  </ThemeProvider>
 )
 
 export default Index
